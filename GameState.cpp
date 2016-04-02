a5e5d4ca9d7fc34e841da889c0efa8676535e710
Michał Martyniak <michal.martyniak@linux.pl>
2016-04-02 12:43:54 +0100

    Still rebuilding

--- GameState.cpp
+++ GameState.cpp
@@ -1,14 +1,25 @@
 #include "GameState.h"
+#include<iostream>
+using namespace std;
 
+vector<int> GameState::gameBoard(N + 6, 1);
+vector<GameState> GameState::queue;
 
 GameState::GameState(int g, int a, int b)
 {
+	this->g = g;
+	this->a = a;
+	this->b = b;
 
+	c = 0;//intercept equals 0 at the beggining
+				//further some fractions will be added to that variable
+	
+	fillEquation();
 }
-
 
 GameState::~GameState()
 {
+	//nothing to deallocate
 }
 
 bool operator==(const GameState& l, const GameState& r)
@@ -20,4 +31,55 @@ bool operator==(const GameState& l, const GameState& r)
 		return true;
 	else
 		return false;
+}
+
+void GameState::fillEquation()
+{
+	printf("x(%d, %d, %d) = ", g, a, b);
+	int e = g == 1 ? 2 : 1;			//e - enemy
+
+	//testing dice rolling result
+	for (int d = 1; d <= 6; d++)	//d - dice roll result
+	{
+		if (g == 1)
+		{
+			if (gameBoard[a + d] != END_OF_BOARD)
+			{
+				int penalty = gameBoard[a + d] < 0 ? gameBoard[a + d] : 0;
+				GameState unknownState = GameState(e, a + d + penalty, b);
+
+				//if no identical unknown game state is found, then add it to our queue
+				if (find(queue.begin(), queue.end(), unknownState) == queue.end())
+					queue.push_back(unknownState);
+
+				printf("1/6*(%d, %d, %d) + ", e, a + d + penalty, b);
+			}//else - if player 'g' is out of board, he WINS
+			else
+			{	
+				c += 1 / 6;			//
+				printf("1/6 + ");	//1 means that player nr 1 won
+			}
+		}
+		if (g == 2)
+		{
+			if (gameBoard[b + d] != END_OF_BOARD)
+			{
+				int penalty = gameBoard[b + d] < 0 ? gameBoard[b + d] : 0;
+				GameState unknownState = GameState(e, a, b + d + penalty);
+
+				//if no identical unknown game state is found, then add it to our queue
+				if (find(queue.begin(), queue.end(), unknownState) == queue.end())
+					queue.push_back(unknownState);
+
+				printf("1/6*(%d, %d, %d) + ", e, a, b + d + penalty);
+			}//else - if player 'g' is out of board, he WINS
+			else
+			{
+				c += 0;
+				printf("0/6 + ");//0 means that player nr 1 defeated
+			}
+		}
+
+	}
+	cout << "\b\b\b\033[K" << endl;//deletes excessive ' + 'sign
 }
--- GameState.h
+++ GameState.h
@@ -1,4 +1,10 @@
 #pragma once
+#include<vector>
+using namespace std;
+
+#define N 3		//board length
+#define END_OF_BOARD 0//value that means - 'if you are here, you won'
+
 class GameState
 {
 private:
@@ -6,9 +12,14 @@ private:
 	int a;	//1st player position
 	int b;	//2nd player position
 	int c;	//intercept
+	vector<double> equation;//holds factors(PL wspó³czynniki)
 public:
+	static vector<int> gameBoard;//na zapas zeby nie wyjsc za zakres
+	static vector<GameState> queue;
+
 	GameState(int g, int a, int b);
 	~GameState();
+	void fillEquation();
 
 	friend bool operator==(const GameState& l, const GameState& p);
 };
